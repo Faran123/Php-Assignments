4cda103c99ef77ffc8958c081aa5e6ba3059c4e5
Faran123 <bcs.012.025@gmail.com>
2018-06-19 07:25:16 +0500

    code has been modified

--- Task1/index.php
+++ Task1/index.php
@@ -1,42 +1,54 @@
 <?php
 
+readCsvFile("input.csv", "output.csv");
 /**
- * @param $path
- * @param $outputFilePath
+ * Read data from excel file
+ * @param string $input_file_path
+ * @param string $output_file_path
  */
-function readCsvFile($path, $outputFilePath)
+function readCsvFile($input_file_path, $output_file_path)
 {
-    $file = fopen($path, "r");
+    $input_file = fopen($input_file_path, "r");
     try {
-        if (file_exists($path) == false) {
-            throw  new Exception("File not exists!");
+        if (file_exists($input_file_path) == false) {
+            throw  new Exception("Input file does not exist!");
         } else {
-            writeToCsvFile($file, $outputFilePath);
-            fclose($file);
+            writeToCsvFile($input_file, $output_file_path);
+            fclose($input_file);
         }
-    } catch (Exception $e) {
-            echo "<b>". $e->getMessage(). "</b>";
+    } catch (Exception $file_exception) {
+            echo "<b>". $file_exception->getMessage(). "</b>";
     }
 }
 
 /**
- * @param $file
- * @param $path
- */    
-function writeToCsvFile($file, $path)
+ * Write data to excel file
+ * @param file $input_file
+ * @param string $output_file_path
+ */
+function writeToCsvFile($input_file, $output_file_path)
 {
-    $write_file = fopen($path, "w+");
-    while (($data = fgetcsv($file, filesize("input.csv"), ",")) !== false) {
-        foreach ($data as $key => $value) {
-            $match = preg_match("/^([0-9]+[a-z]|[a-z]+[0-9])[a-zA-Z0-9]*@(coeus-solutions.de)$/", $value);
-            if ($match == 1) {
-                $output_data = $data[0].",". $data[1]. ",". $data[3].",". $data[2].",";
-                echo $output_data. "<br>";
-                if (!fputcsv($write_file, explode(',', $output_data))) {
-                    echo "<br> Something went wrong writing data to file $data";
-                }
+    $write_file = fopen($output_file_path, "w+");
+    while (($data = fgetcsv($input_file, filesize("input.csv"), ",")) !== false) {
+        $match = emailValidation($data[3]);
+        if ($match) {
+            $output_data = $data[0].",". $data[1]. ",". $data[3].",". $data[2].",";
+            echo $output_data. "<br>";
+            if (!fputcsv($write_file, explode(',', $output_data))) {
+                echo "<br> Something went wrong writing data to file";
             }
         }
     }
+    fclose($write_file);
 }
-    readCsvFile("input.csv", "output.csv");
+
+/**
+ * performs email validation
+ * @param string $email
+ * @return false|true
+ */
+function emailValidation($email)
+{
+    $match = preg_match("/^([0-9]+[a-z]|[a-z]+[0-9])[a-zA-Z0-9]*@(coeus-solutions.de)$/", $email);
+    return $match;
+}
