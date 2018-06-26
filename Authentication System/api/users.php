bc05e1c5c23b8f85294830c2ea062baf2305422a
Faran123 <bcs.012.025@gmail.com>
2018-06-26 06:25:03 +0500

    Api is implemented in order to read users details

---
+++ Authentication System/api/users.php
@@ -0,0 +1,47 @@
+<?php
+include "../db_connection.php";
+include "../helper_functions.php";
+retrieveUsers($connection);
+
+/**
+ * This method retrieves user details from users table
+ * @param $connection
+ */
+function retrieveUsers($connection)
+{
+    try {
+        $result = $connection->query("SELECT email, status, activation_code, user_type from users");
+        if ($result == false) {
+            throw new Exception(exceptionMessage(""));
+        } else {
+            if ($result->num_rows > 0) {
+                $usersArray = displayUsers($result);
+                echo $usersArray;
+            } else {
+                echo "User not found!";
+            }
+        }
+    } catch (Exception $exception) {
+        echo $exception->getMessage();
+    }
+}
+
+/**
+ * This method displays users details as an json array
+ * @param $result
+ * @return string
+ */
+function displayUsers($result)
+{
+    $users["body"] = array();
+    while ($data = $result->fetch_assoc()) {
+        $usersArray = array(
+            "email" => $data["email"],
+            "status" => $data["status"],
+            "activation code" => $data["activation_code"],
+            "user type" => $data["user_type"],
+        );
+        array_push($users["body"], $usersArray);
+    }
+    return json_encode($users);
+}
